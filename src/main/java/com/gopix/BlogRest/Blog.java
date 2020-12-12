2a29a5eafb58ec66375c63dab61929ebecc440ec
Manan <mananbhanushali98@gmail.com>
2020-12-12 15:48:14 +0530

    Add config files

---
+++ src/main/java/com/gopix/BlogRest/Blog.java
@@ -0,0 +1,17 @@
+package com.gopix.BlogRest;
+
+public class Blog {
+	private int id;
+	private String title;
+	private String content;
+	
+	public Blog() { }
+	public Blog (int id,String title,String content) {
+		this.setID(id);
+		this.setID(title);
+		this.setID(content);
+		
+		
+	}
+
+}
