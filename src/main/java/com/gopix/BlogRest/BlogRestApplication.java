76490d05dd294b597c8ed9470231acf4230e49a9
Manan <mananbhanushali98@gmail.com>
2020-12-12 15:32:02 +0530

    Blog rest api skeleton

---
+++ src/main/java/com/gopix/BlogRest/BlogRestApplication.java
@@ -0,0 +1,13 @@
+package com.gopix.BlogRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //annotation to the class to make it a Spring Boot application. You can now run the application.
+public class BlogRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestApplication.class, args);
+	}
+
+}
