e561ab6b6a32c3f2bed5e9240229225ee5fb1d70
Manan <mananbhanushali98@gmail.com>
2020-12-20 15:18:11 +0530

    Add dummy data to test the apis

--- src/main/java/com/gopix/BlogRest/Blog.java
+++ src/main/java/com/gopix/BlogRest/Blog.java
@@ -8,10 +8,42 @@ public class Blog {
 	public Blog() { }
 	public Blog (int id,String title,String content) {
 		this.setID(id);
-		this.setID(title);
-		this.setID(content);
+		this.setTitle(title);
+		this.setContent(content);
 		
 		
+	}
+	
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void setID(int id) {
+		this.id=id;
+	}
+	
+	public String getTitle(){
+		return title;
+	}	
+	
+	public void setTitle(String title) {
+		this.title = title;
+		
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content= content;
+	}
+	
+	
+	@Override
+	public  String toString() {
+		return "Blog={" + "id =" +id +"title= "+ title+"/"+"content="+content +'/'+"}";
 	}
 
 }
